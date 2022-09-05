2a2ea3d0f04d636bbe333bf71b08b3a140caecd0
GraficPixel <thschwarz725@gmail.com>
2022-09-05 18:36:35 +0200

    Fehler mit + 1 bei Z. 18 & 19 behoben

--- exercises/primzahlen.py
+++ exercises/primzahlen.py
@@ -5,18 +5,18 @@ prim = []
 def main():
     print('''HINWEIS: Die Primzahlen von 1 bis 100'000, mit diesem Code zu berechnen dauert ca. 1079 Sekunden (17:59.147),
 die Primzahlen von 1 bis 10'000 jedoch brauchen lediglich ca. 11 Sekunden (00:11.274).'''
-    )
+          )
     N = int(input('Die Primzahlen von 1 bis wieviel willst du berechnet haben? '))
     start_time = time()
-    for i in range(1, N + 1): # erzeugt eine iterierbare Liste von 1 bis und mit N
+    for i in range(1, N + 1):  # erzeugt eine iterierbare Liste von 1 bis und mit N
         zähler = 0
-        for w in range(1, N + 1): # erzeugt eine iterierbare Liste von 1 bis und mit N
+        for w in range(1, N + 1):  # erzeugt eine iterierbare Liste von 1 bis und mit N
             x = i % w
             if x != 0:
                 zähler = zähler + 1
         if zähler == (N - 2):
-            print(i + 1, " - ", round((i / N) * 100, 2), "%")
-            prim.append(i + 1)
+            print(i, " - ", round((i / N) * 100, 2), "%")
+            prim.append(i)
     end_time = time()
     process_time = round((end_time - start_time), 3)
     print(f'Die Primzahlen von 1 bis {N} sind:')
