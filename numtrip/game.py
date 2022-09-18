4101b7b7c5be06d3504be687b994f2724bad2b80
GraficPixel <thschwarz725@gmail.com>
2022-09-18 20:41:20 +0200

    Zahlen werden jetzt farbig dargestellt

--- numtrip/game.py
+++ numtrip/game.py
@@ -1,7 +1,8 @@
 from random import randint
 from colorama import init
 from math import*
-MAX_POTENZ = 6
+MAX_POTENZ = 5
+col = 0
 spiel = [[(2**(randint(1, MAX_POTENZ+1))) for i in range(5)] for i in range(5)]
 anzeige = [[],[],[],[],[]] 
 print(spiel)
@@ -13,12 +14,18 @@ for i in range(5):
             anzeige[i].append(f" {spiel[i][j]} ")
         elif spiel[i][j] < 1000:
             anzeige[i].append(f"{spiel[i][j]} ")
-        print(spiel[i][j])
 print('╔══════╤══════╤══════╤══════╤══════╗')
 for i in range(5):
+    p1 = int(log(int(anzeige[i][0])) / log(2) + 30)
+    p2 = int(log(int(anzeige[i][1])) / log(2) + 30)
+    p3 = int(log(int(anzeige[i][2])) / log(2) + 30)
+    p4 = int(log(int(anzeige[i][3])) / log(2) + 30)
+    p5 = int(log(int(anzeige[i][4])) / log(2) + 30)
     print("║      │      │      │      │      ║")
-    print("║",anzeige[i][0],"│", anzeige[i][1],"│",anzeige[i][2],"│",anzeige[i][3],"│",anzeige[i][4],"║")
+    print("║",f'\033[{p1}m' + anzeige[i][0], '\033[39m' + "│", f'\033[{p2}m' + anzeige[i][1],'\033[39m' + "│",f'\033[{p3}m' + anzeige[i][2],'\033[39m' + "│",f'\033[{p4}m' + anzeige[i][3],'\033[39m' + "│",f'\033[{p5}m' + anzeige[i][4],'\033[39m' + "║")
     print("║      │      │      │      │      ║")
     if i < 4:
         print("╟──────┼──────┼──────┼──────┼──────╢")
 print("╚══════╧══════╧══════╧══════╧══════╝")
+
+# log(zahl) / log(exponent)
