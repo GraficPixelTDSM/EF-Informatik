54f0d6b3d637df64bac845cd1b7f1215e215f951
GraficPixel <thschwarz725@gmail.com>
2022-10-28 16:28:33 +0200

    Bedingung erweitert

--- numtrip/game.py
+++ numtrip/game.py
@@ -1,10 +1,10 @@
 from random import randint
 from colorama import init
-from math import*
+from math import *
 MAX_POTENZ = 5
 col = 0
-spiel = [[(2**(randint(1, MAX_POTENZ+1))) for i in range(5)] for i in range(5)]
-anzeige = [[],[],[],[],[]] 
+spiel = [[(2**(randint(1, MAX_POTENZ + 1))) for i in range(5)] for i in range(5)]
+anzeige = [[], [], [], [], []]
 print(spiel)
 for i in range(5):
     for j in range(5):
@@ -14,15 +14,16 @@ for i in range(5):
             anzeige[i].append(f" {spiel[i][j]} ")
         elif spiel[i][j] < 1000:
             anzeige[i].append(f"{spiel[i][j]} ")
+        else:
+            anzeige[i].append(f"{spiel[i][j]}")
 print('╔══════╤══════╤══════╤══════╤══════╗')
 for i in range(5):
-    p1 = int(log(int(anzeige[i][0])) / log(2) + 30)
-    p2 = int(log(int(anzeige[i][1])) / log(2) + 30)
-    p3 = int(log(int(anzeige[i][2])) / log(2) + 30)
-    p4 = int(log(int(anzeige[i][3])) / log(2) + 30)
-    p5 = int(log(int(anzeige[i][4])) / log(2) + 30)
+    def logg(pn):
+        pn = int(log(int(anzeige[i][(pn - 1)])) / log(2) + 30)
+        return pn
     print("║      │      │      │      │      ║")
-    print("║",f'\033[{p1}m' + anzeige[i][0], '\033[39m' + "│", f'\033[{p2}m' + anzeige[i][1],'\033[39m' + "│",f'\033[{p3}m' + anzeige[i][2],'\033[39m' + "│",f'\033[{p4}m' + anzeige[i][3],'\033[39m' + "│",f'\033[{p5}m' + anzeige[i][4],'\033[39m' + "║")
+    print("║", f'\033[{logg(1)}m' + anzeige[i][0], '\033[39m' + "│", f'\033[{logg(2)}m' + anzeige[i][1], '\033[39m' + "│", f'\033[{logg(3)}m' +
+          anzeige[i][2], '\033[39m' + "│", f'\033[{logg(4)}m' + anzeige[i][3], '\033[39m' + "│", f'\033[{logg(5)}m' + anzeige[i][4], '\033[39m' + "║")
     print("║      │      │      │      │      ║")
     if i < 4:
         print("╟──────┼──────┼──────┼──────┼──────╢")
