d5a1d85acfcb8158e43ebb03d99393f5a8035221
GraficPixel <thschwarz725@gmail.com>
2022-11-11 15:55:06 +0100

    Spielfeld Zahlen für Zeilen und Farben geändert

--- numtrip/game.py
+++ numtrip/game.py
@@ -6,6 +6,8 @@ col = 0
 spiel = [[(2**(randint(1, MAX_POTENZ + 1))) for i in range(5)] for i in range(5)]
 anzeige = [[], [], [], [], []]
 print(spiel)
+print('\033[93m' + 'Willkommen zu Numtrip! Klicke auf eine Zahl um alle anliegenden, gleichen Zahlen auf diesen Block zusammenzufassen.\nDabei wird der Wert der angeklickten Zahl verdoppelt. Es werden ständig neue Zahlen kommen, sobald du einige zusammenfasst. \nVersuche eine höchstmögliche Zahl zu erreichen!' +
+      '\033[39m' + '\n\n════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════\n')
 for i in range(5):
     for j in range(5):
         if spiel[i][j] < 10:
@@ -16,15 +18,15 @@ for i in range(5):
             anzeige[i].append(f"{spiel[i][j]} ")
         else:
             anzeige[i].append(f"{spiel[i][j]}")
-print('    01     02     03     04     05   ')
+print('\033[35m' + '    01     02     03     04     05   ' + '\033[39m')
 print(' ╔══════╤══════╤══════╤══════╤══════╗')
 for i in range(5):
     def logg(pn):
-        pn = int(log(int(anzeige[i][(pn - 1)])) / log(2) + 30)
+        pn = int(log(int(anzeige[i][(pn - 1)])) / log(2) + 90)
         return pn
     print(" ║      │      │      │      │      ║")
-    print(str(i + 1) + "║", f'\033[{logg(1)}m' + anzeige[i][0], '\033[39m' + "│", f'\033[{logg(2)}m' + anzeige[i][1], '\033[39m' + "│", f'\033[{logg(3)}m' +
-          anzeige[i][2], '\033[39m' + "│", f'\033[{logg(4)}m' + anzeige[i][3], '\033[39m' + "│", f'\033[{logg(5)}m' + anzeige[i][4], '\033[39m' + "║")
+    print('\033[35m' + str(i + 1) + '\033[39m' + "║", f'\033[1;{logg(1)}m' + anzeige[i][0], '\033[0;39m' + "│", f'\033[1;{logg(2)}m' + anzeige[i][1], '\033[0;39m' + "│", f'\033[1;{logg(3)}m' +
+          anzeige[i][2], '\033[0;39m' + "│", f'\033[1;{logg(4)}m' + anzeige[i][3], '\033[0;39m' + "│", f'\033[1;{logg(5)}m' + anzeige[i][4], '\033[0;39m' + "║")
     print(" ║      │      │      │      │      ║")
     if i < 4:
         print(" ╟──────┼──────┼──────┼──────┼──────╢")
@@ -32,3 +34,4 @@ print(" ╚══════╧══════╧══════╧══
 
 # log(zahl) / log(exponent)
 # TDSM
+# ANSI Code '\033[X;XXm' for Color: https://gist.github.com/Prakasaka/219fe5695beeb4d6311583e79933a009
