bead0ca4398907fc08aef714a371ab63f43a9682
GraficPixel <thschwarz725@gmail.com>
2022-11-18 22:45:55 +0100

    make game work, add better input evaluation system

--- numtrip/game.py
+++ numtrip/game.py
@@ -5,7 +5,9 @@ MAX_POTENZ_START = 3
 max_potenz_var = 2
 col = 0
 Game_Over = False
+global spiel
 spiel = [[(2**(randint(1, MAX_POTENZ_START + 1))) for i in range(5)] for i in range(5)]
+
 anzeige = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
 check = [[0], [0]]
 #check= [[y], [x]]
@@ -44,82 +46,99 @@ def field_print():
 field_print()
 
 
-def game():
-    while Game_Over is False:
-        def check_num_input(inp):
-            if inp.isnumeric():
-                if int(inp) <= 5 and int(inp) >= 1:
-                    return True
-                else:
-                    print('\033[91m' + 'Bitte schreibe eine Zahl zwischen 1 und 5!' + '\033[0m')
-                    return False
-
+while Game_Over is False:
+    def check_num_input(inp):
+        if inp.isnumeric():
+            if int(inp) <= 5 and int(inp) >= 1:
+                return True
             else:
-                print('\033[91m' + 'Bitte schreibe eine Zahl!' + '\033[0m')
+                print('\033[91m' + 'Bitte schreibe eine Zahl zwischen 1 und 5!' + '\033[0m')
                 return False
 
+        else:
+            print('\033[91m' + 'Bitte schreibe eine Zahl!' + '\033[0m')
+            return False
+    global x_a
+    global y_a
+    x_a = input('Welche Position auf der X-Achse willst du auswählen? ')
+    while check_num_input(x_a) is False:
         x_a = input('Welche Position auf der X-Achse willst du auswählen? ')
-        while check_num_input(x_a) is False:
-            x_a = input('Welche Position auf der X-Achse willst du auswählen? ')
-        check[0].append(x_a)
+    check[0].append(x_a)
+    y_a = input('Welche Position auf der Y-Achse willst du auswählen? ')
+    while check_num_input(y_a) is False:
         y_a = input('Welche Position auf der Y-Achse willst du auswählen? ')
-        while check_num_input(y_a) is False:
-            y_a = input('Welche Position auf der Y-Achse willst du auswählen? ')
-        check[1].append(y_a)
-        print('\033[92m' + f'Ausgewählte Koordinate: (' + x_a + ';' + y_a + ')' + '\033[0m')
+    check[1].append(y_a)
+    print('\033[92m' + f'Ausgewählte Koordinate: (' + x_a + ';' + y_a + ')' + '\033[0m')
+    x_a = int(x_a) - 1
+    y_a = int(y_a) - 1
+    print(len(check[0]))
+    tempa = 0
+    while len(check[0]) > 1:
+        tempa = +1
+        for lenc in range(len(check[0]) - 1):
+            global isit
+            isit = 0
+            x = int(check[0][1]) - 1
+            y = int(check[1][1]) - 1
+            if tempa < 2:
+                anz3 = int(anzeige[y_a][x_a].strip())
 
-        print(len(check[0]))
-
-        while len(check[0]) > 1:
-            for lenc in range(len(check[0]) - 1):
-                global isit
-                isit = 0
-                global tempx
-                global tempy
-                x = int(check[0][lenc + 1]) - 1
-                y = int(check[1][lenc + 1]) - 1
-                tempx = x
-                tempy = y
-                for i in range(len(check) - 1):
-                    check[0].pop(-(i + 1))
-                    check[1].pop(-(i + 1))
-                if x < 4:
-                    if spiel[y][x] == spiel[y][x + 1]:
+            check[0].pop(1)
+            check[1].pop(1)
+            if x < 4:
+                anz1 = int(anzeige[y][x + 1].strip())
+                anz2 = int(anzeige[y][x].strip())
+                if anz2 == anz1:
+                    if spiel[y][x + 1] != 0:
+                        check[0].append(x + 2)
+                        check[1].append(y + 1)
                         spiel[y][x + 1] = 0
                         isit = +1
                         print('same right')
-                if x > 0:
-                    if spiel[y][x] == spiel[y][x - 1]:
+            if x > 0:
+                anz1 = int(anzeige[y][x - 1].strip())
+                anz2 = int(anzeige[y][x].strip())
+                if anz2 == anz1:
+                    if spiel[y][x - 1] != 0:
+                        check[0].append(x)
+                        check[1].append(y + 1)
                         spiel[y][x - 1] = 0
                         isit = +1
                         print('same left')
-                if y < 4:
-                    if spiel[y][x] == spiel[y + 1][x]:
+            if y < 4:
+                anz1 = int(anzeige[y + 1][x].strip())
+                anz2 = int(anzeige[y][x].strip())
+                if anz2 == anz1:
+                    if spiel[y + 1][x] != 0:
+                        check[0].append(x + 1)
+                        check[1].append(y + 2)
                         spiel[y + 1][x] = 0
                         isit = +1
                         print('same down')
-                if y > 0:
-                    if spiel[y][x] == spiel[y - 1][x]:
+            if y > 0:
+                anz1 = int(anzeige[y - 1][x].strip())
+                anz2 = int(anzeige[y][x].strip())
+                if anz2 == anz1:
+                    if spiel[y - 1][x] != 0:
+                        check[0].append(x + 1)
+                        check[1].append(y)
                         spiel[y - 1][x] = 0
                         isit = +1
                         print('same up')
+    spiel[y_a][x_a] = anz3 * 2
 
-        if isit > 0:
-            spiel[tempy][tempx] = spiel[tempy][tempx] * 2
-        while 0 in spiel[0] or 0 in spiel[1] or 0 in spiel[2] or 0 in spiel[3] or 0 in spiel[4]:
-            for l in range(5):
-                for m in range(5):
-                    if spiel[l][m] == 0:
-                        if l == 0:
-                            spiel[l][m] = 2**randint(1, max_potenz_var)
-                        else:
-                            spiel[l][m] = spiel[l - 1][m]
-                            spiel[l - 1][m] = 0
+    while 0 in spiel[0] or 0 in spiel[1] or 0 in spiel[2] or 0 in spiel[3] or 0 in spiel[4]:
+        for l in range(5):
+            for m in range(5):
+                if spiel[l][m] == 0:
+                    if l == 0:
+                        spiel[l][m] = 2**randint(1, max_potenz_var)
+                    else:
+                        spiel[l][m] = spiel[l - 1][m]
+                        spiel[l - 1][m] = 0
 
-        field_print()
+    field_print()
 
-
-game()
 
 # log(zahl) / log(exponent)
 # TDSM
