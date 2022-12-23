6485d5b76a188de22a0b281558db973e66334356
GraficPixel <thschwarz725@gmail.com>
2022-12-23 16:38:51 +0100

    add function to save the current and load the previous game

--- numtrip/game.py
+++ numtrip/game.py
@@ -1,20 +1,39 @@
 from random import randint
 from math import log
+import json
+daten = {}
 MAX_POTENZ_START = 2
 MAX_POTENZ_VAR = 2
 WIN_NUM = 128
 Game_Over = False
 ok = 0
 wl = 0
+dateiname = 'numtrip.json'
+# Gespeichert: spiel, wl, ok
+daten = {}
 spiel = [[(2**(randint(1, MAX_POTENZ_START + 1))) for i in range(5)] for i in range(5)]
 anzeige = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
 check = [[0], [0]]
 #check= [[y], [x]]
+
+load_game = input(
+    '[?] Willst du die vorherige Sitzung laden oder ein neues Spiel anfangen? (Laden: L; Neues Spiel: N) ').lower()
+while load_game != 'l' and load_game != 'n':
+    load_game = input(
+        '[?] Willst du die vorherige Sitzung laden oder ein neues Spiel anfangen? (Laden: L; Neues Spiel: N) ')
+if load_game == 'l':
+    try:
+        with open(dateiname) as f:
+            daten = json.load(f)
+            spiel = daten['spiel']
+    except:
+        print('[!] Da leider kein gespeichertes Spiel gefunden wurde, wurde ein neues erstellt.')
 print('\n' + '\033[33m' + '════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════\n\n' + '\033[96m' +
-      f'Willkommen zu Numtrip! Klicke auf eine Zahl um alle anliegenden, gleichen Zahlen auf diesen Block zusammenzufassen.\nDabei wird der Wert der angeklickten Zahl verdoppelt. Es werden ständig neue Zahlen kommen, sobald du einige zusammenfasst. \nVersuche die Zahl {WIN_NUM} zu erreichen, oder spiele danach im unbegrenzten Modus weiter.\nViel Glück!\n-GPTDSM' + '\033[33m' + '\n\n════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════\n')
+      f'Willkommen zu Numtrip! Klicke auf eine Zahl um alle anliegenden, gleichen Zahlen auf diesen Block zusammenzufassen.\nDabei wird der Wert der angeklickten Zahl verdoppelt. Es werden ständig neue Zahlen kommen, sobald du einige zusammenfasst. \nVersuche die Zahl {WIN_NUM} zu erreichen, oder spiele danach im unbegrenzten Modus weiter.\nDas Spiel speichert automatisch nach jeder Eingabe und kann jederzeit beendet und fortgesetzt werden.\nViel Glück!\n-GPTDSM' + '\033[33m' + '\n\n════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════\n')
 
 
 def field_print():
+    global daten
     for i in range(5):
         for j in range(5):
             global spiel
@@ -33,12 +52,16 @@ def field_print():
         if i < 4:
             print(" ╟──────┼──────┼──────┼──────┼──────╢")
     print(" ╚══════╧══════╧══════╧══════╧══════╝")
+    daten['spiel'] = spiel
+    with open(dateiname, 'w') as f:
+        json.dump(daten, f)
 
 
 field_print()
 
 
 def main():
+    global Game_Over
     Game_Over = False
     while Game_Over is False:
         def check_num_input(inp):
@@ -46,26 +69,26 @@ def main():
                 if int(inp) <= 5 and int(inp) >= 1:
                     return True
                 else:
-                    print('\033[91m' + 'Bitte schreibe eine Zahl zwischen 1 und 5!' + '\033[0m')
+                    print('\033[91m' + '[!] Bitte schreibe eine Zahl zwischen 1 und 5!' + '\033[0m')
                     return False
             else:
-                print('\033[91m' + 'Bitte schreibe eine (positive) Zahl!' + '\033[0m')
+                print('\033[91m' + '[!] Bitte schreibe eine (positive) Zahl!' + '\033[0m')
                 return False
         global x_a
         global y_a
         global wl
-        x_a = input('Welche Position auf der X-Achse willst du auswählen? ')
+        x_a = input('[?] Welche Position auf der X-Achse willst du auswählen? ')
         while check_num_input(x_a) is False:
-            x_a = input('Welche Position auf der X-Achse willst du auswählen? ')
+            x_a = input('[?] Welche Position auf der X-Achse willst du auswählen? ')
         check[0].append(x_a)
-        y_a = input('Welche Position auf der Y-Achse willst du auswählen? ')
+        y_a = input('[?] Welche Position auf der Y-Achse willst du auswählen? ')
         while check_num_input(y_a) is False:
-            y_a = input('Welche Position auf der Y-Achse willst du auswählen? ')
+            y_a = input('[?] Welche Position auf der Y-Achse willst du auswählen? ')
         check[1].append(y_a)
         if wl == 0:
-            print('\033[92m' + 'Ausgewählte Koordinate: (' + x_a + ';' + y_a + ')' + '\033[0m')
+            print('\033[92m' + '[!] Ausgewählte Koordinate: (' + x_a + ';' + y_a + ')' + '\033[0m')
         elif wl == 1:
-            print('\033[92m' + 'Ausgewählte Koordinate: (' + x_a + ';' + y_a + ')' +
+            print('\033[92m' + '[!] Ausgewählte Koordinate: (' + x_a + ';' + y_a + ')' +
                   '\033[91m' + ' (Unbegrenzter Modus)' + '\033[0m')
         x_a = int(x_a) - 1
         y_a = int(y_a) - 1
@@ -136,31 +159,40 @@ def main():
                             spiel[l][m] = spiel[l - 1][m]
                             spiel[l - 1][m] = 0
         wl = 0
+        if load_game == 'l':
+            try:
+                global ok
+                with open(dateiname) as f:
+                    daten = json.load(f)
+                    ok = daten['ok']
+            except:
+                ok = 0
         if ok == 0:
             for l in range(5):
                 if WIN_NUM in spiel[l]:
-                    wl = 1
                     Game_Over = True
         field_print()
 
 
 main()
 if Game_Over == True:
-    if wl == 1:
+    weiter = input(
+        '\033[93m' + f'[?] Gratulation! Du hast ein Feld auf die erforderliche Punktzahl von {WIN_NUM} gebracht.\nWillst du im unbegrenzten Modus weiterspielen? (j/n)\n' + '\033[0m')
+    weiter = weiter.lower()
+    while weiter != 'ja' and weiter != 'j' and weiter != 'y' and weiter != 'yes' and weiter != 'nein' and weiter != 'n' and weiter != 'no':
+        print('\033[91m' + "[!] Bitte gib eine gültige Antwort ein! (Gültige Antworten: 'j', 'ja', 'y', 'yes', 'n', 'nein', 'no')" + '\033[0m')
         weiter = input(
-            '\033[93m' + f'Gratulation! Du hast ein Feld auf die erforderliche Punktzahl von {WIN_NUM} gebracht.\nWillst du im unbegrenzten Modus weiterspielen? (j/n)\n' + '\033[0m')
+            '\033[93m' + f'[?] Gratulation! Du hast ein Feld auf die erforderliche Punktzahl von {WIN_NUM} gebracht.\nWillst du im unbegrenzten Modus weiterspielen? (j/n)\n' + '\033[0m')
         weiter = weiter.lower()
-        while weiter != 'ja' and weiter != 'j' and weiter != 'y' and weiter != 'yes' and weiter != 'nein' and weiter != 'n' and weiter != 'no':
-            print('\033[91m' + "Bitte gib eine gültige Antwort ein! (Gültige Antworten: 'j', 'ja', 'y', 'yes', 'n', 'nein', 'no')" + '\033[0m')
-            weiter = input(
-                '\033[93m' + f'Gratulation! Du hast ein Feld auf die erforderliche Punktzahl von {WIN_NUM} gebracht.\nWillst du im unbegrenzten Modus weiterspielen? (j/n)\n' + '\033[0m')
-            weiter = weiter.lower()
 
-        if weiter == 'ja' or weiter == 'j' or weiter == 'y' or weiter == 'yes':
-            ok = 1
-            main()
-        elif weiter == 'nein' or weiter == 'n' or weiter == 'no':
-            Game_Over = True
-            print('\033[94m' + "\nDanke für's Spielen von Numtrip.\n-GPTDSM\n" + '\033[0m')
+    if weiter == 'ja' or weiter == 'j' or weiter == 'y' or weiter == 'yes':
+        ok = 1
+        daten['ok'] = ok
+        with open(dateiname, 'w') as f:
+            json.dump(daten, f)
+        main()
+    elif weiter == 'nein' or weiter == 'n' or weiter == 'no':
+        Game_Over = True
+        print('\033[94m' + "\n[!] Danke für's Spielen von Numtrip.\n-GPTDSM\n" + '\033[0m')
 # TDSM
 # ANSI Code '\033[X;XXm' for Color: https://gist.github.com/Prakasaka/219fe5695beeb4d6311583e79933a009
